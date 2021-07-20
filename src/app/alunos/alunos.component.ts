281bd4a7021519a41d760b262f919ec6f638367a
Breno Robert <64442888+BrenoRobertDiogo@users.noreply.github.com>
2021-07-20 14:39:13 -0300

    sla tlgd

---
+++ src/app/alunos/alunos.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos',
+  templateUrl: './alunos.component.html',
+  styleUrls: ['./alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
+  
+  public titulo: string = 'Alunos';
+  public alunoSelecionado: string = '';
+
+  public alunos: Array<any> = [
+    { id: 1, nome: "Marta", sobrenome: "Kent",    telefone: 332255, }, // 1
+    { id: 2, nome: "Paula", sobrenome: "Isabela", telefone: 332255, }, // 2
+    { id: 3, nome: "Laura", sobrenome: "Antonia", telefone: 332255, }, // 3
+    { id: 4, nome: "Luiza", sobrenome: "Maria",   telefone: 332255, }, // 4
+    { id: 5, nome: "Lucas", sobrenome: "Machado", telefone: 332255, }, // 5
+    { id: 6, nome: "Pedro", sobrenome: "Alvares", telefone: 332255, }, // 6
+    { id: 7, nome: "Paulo", sobrenome: "José",    telefone: 332255, }  // 7
+  ]
+
+  alunoSelect(aluno: any) {
+    this.alunoSelecionado = aluno.nome
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/professores/professores.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professores',
+  templateUrl: './professores.component.html',
+  styleUrls: ['./professores.component.css']
+})
+export class ProfessoresComponent implements OnInit {
+  public titulo = 'Professores'
+
+  public professores: Array<any> = [
+    { id: 1,nome: "Lauro",     disciplina: "Matemática"  },
+    { id: 2,nome: "Roberto",   disciplina: "Física"      },
+    { id: 3,nome: "Ronaldo",   disciplina: "Português"   },
+    { id: 4,nome: "Rodrigo",   disciplina: "Inglês"      },
+    { id: 5,nome: "Alexandre", disciplina: "Programação" },
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
